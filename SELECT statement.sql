f9d820fb5faa06d3236f81857c80b3ce3fb6ed58
Anand <99013863+AnandSavvy@users.noreply.github.com>
2022-02-17 19:53:20 +0530

    Add files via upload

---
+++ SELECT statement.sql
@@ -0,0 +1,6 @@
+select * from sakila.actor  
+select actor_id,first_name from sakila.actor;
+select last_name from sakila.actor;
+select last_update from sakila.actor;
+select * from sakila.actor where actor_id >100;
+select * from sakila.actor where actor_id>110;
