2979eed2fb90004abdacc13d3b3c0f76afc83bd1
Anand <99013863+AnandSavvy@users.noreply.github.com>
2022-02-17 19:53:50 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,19 @@
+create database demo;
+
+use demo;
+create table test (id int,name varchar(20),marks int,class int);
+insert into test values(1,'arya',95,10);
+insert into test values(2,'evander',100,10);
+insert into test values(3,'henry',80,10);
+insert into test values(4,'idris',95,10);
+insert into test values(5,'james',92,10);
+insert into test values(6,'kai',72,10);
+insert into test values(7,'jacob',49,10);
+insert into test values(8,'linne',100,10);
+insert into test values(9,'kim',10,10);
+insert into test values(10,'linni',59,10);
+insert into test values(11'malai',85,10);
+
+
+
+select * from test;
